d4a3554de22f318479c50d6b2d05e5e481c4582e
nacho <76809065+nach8686@users.noreply.github.com>
2024-01-10 21:13:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
-from transformers import pipeline
 import nltk
-from secciones.procesar_textos import procesar_textos  # Asegúrate de que esta ruta sea correcta
+from secciones.procesar_textos import procesar_textos
 from secciones.home_page import home_page
 from secciones.info_page import info_page
 
@@ -20,12 +19,21 @@ def main():
     logo_log_bar = r'imagenes/Captura de pantalla 2024-01-06 a las 17.13.19.png'
     st.set_page_config(page_title='Análisis sentimientos', page_icon=logo_log_bar, layout="wide")
 
-    # Cargar modelos de Hugging Face para análisis de sentimientos y emociones
-    clasificador_sentimiento = pipeline('sentiment-analysis',
-                                        model='citizenlab/twitter-xlm-roberta-base-sentiment-finetunned')
-    clasificador_emociones = pipeline("text-classification",
-                                      model="maxpe/bertin-roberta-base-spanish_sem_eval_2018_task_1")
+    # Descargar recursos de NLTK
+    @st.cache_data
+    def descargar_recursos_nltk():
+        """
+        Descarga los recursos necesarios de NLTK.
 
+        Esta función descarga los componentes 'punkt' y 'stopwords' de NLTK, que son necesarios para tokenizar y
+        preprocesar el texto en las funciones de análisis.
+
+        :return: None.
+        """
+        nltk.download('punkt')
+        nltk.download('stopwords')
+
+    descargar_recursos_nltk()
 
     # Menú de opciones
     with st.sidebar:
@@ -39,21 +47,43 @@ def main():
             "Analizar texto": "🔍 Analizar texto",
             "Info": "ℹ️ Info"
         }
-        # Opción para ingresar texto manualmente
-        text_input = st.text_area("Ingrese su texto aquí:")
+        # Valor predeterminado para 'selected'
+        if 'selected' not in st.session_state:
+            st.session_state['selected'] = "Home"
 
-        # Botón para procesar el texto
-        if st.button("Analizar Texto"):
-            if text_input:
-                # Análisis de sentimiento
-                resultado_sentimiento = clasificador_sentimiento(text_input, truncation=True, max_length=512)
-                st.write("Resultado Sentimiento:", resultado_sentimiento)
+        # Estilo personalizado para los botones
+        btn_style = """
+            <style>
+                .css-2trqyj { 
+                    display: flex; 
+                    justify-content: center; 
+                    align-items: center; 
+                    font-size: 18px; 
+                    font-weight: bold;
+                }
+            </style>
+        """
+        st.markdown(btn_style, unsafe_allow_html=True)
 
-                # Análisis de emociones
-                resultado_emociones = clasificador_emociones(text_input, truncation=True, max_length=512)
-                st.write("Resultado Emociones:", resultado_emociones)
-            else:
-                st.warning("Por favor, ingrese texto.")
+        # Crear botones en la barra lateral para cada opción del menú
+        for opcion, etiqueta in opciones_menu.items():
+            if st.button(etiqueta, key=opcion, use_container_width=True):
+                st.session_state['selected'] = opcion
+
+    # Vista de información
+    if st.session_state['selected'] == "Home":
+        st.image("imagenes/uoc2.png", use_column_width=True)
+        home_page()
+
+    # Analizar texto
+    elif st.session_state['selected'] == "Analizar texto":
+        st.image("imagenes/uoc2.png", use_column_width=True)
+        procesar_textos()
+
+    # Info
+    elif st.session_state['selected'] == "Info":
+        st.image("imagenes/uoc2.png", use_column_width=True)
+        info_page()
 
 
 if __name__ == "__main__":
